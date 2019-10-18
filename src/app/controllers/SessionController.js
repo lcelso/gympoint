53bdf70ce04f24d1ab0d20e0b1ac3692a11de79c
Luiz Celso de Faria Alves <luiz.alves@accenture.com>
2019-10-18 19:13:46 -0300

    Created and configured session files

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken';
+
+import User from '../models/User';
+import authConfig from '../../config/auth';
+
+class SessionController {
+  async store(req, res) {
+    const { email } = req.body;
+    const user = await User.findOne({ where: { email } });
+    const { id, name } = user;
+
+    return res.json({
+      user: {
+        id,
+        name,
+        email,
+      },
+      token: jwt.sign({ id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      }),
+    });
+  }
+}
+
+export default new SessionController();
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
-routes.get('/users', UserController.index);
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.get('/users', UserController.index);
 routes.put('/users', UserController.update);
 routes.delete('/users', UserController.delete);
 
