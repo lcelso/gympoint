48a386addb4325b7ab219f94a2c9882cbeb089eb
Luiz Celso de Faria Alves <luiz.alves@accenture.com>
2019-10-18 18:47:32 -0300

    Created validation with yup and refactored some methods

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,20 +1,40 @@
+import * as Yup from 'yup';
+
 import User from '../models/User';
 
 class UserController {
   async index(req, res) {
     const { email } = req.body;
 
-    const { id, name } = await User.findOne({ where: { email } });
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(400).json({ error: 'User does not exist.' });
+    }
 
     return res.json({
-      id,
-      name,
+      id: user.id,
+      name: user.name,
       email,
     });
   }
 
   async store(req, res) {
     const { email } = req.body;
+    const schemaUser = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
+
+    if (!(await schemaUser.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validations fails.' });
+    }
+
     const user = await User.findOne({ where: { email } });
 
     if (user) {
@@ -31,8 +51,28 @@ class UserController {
   }
 
   async update(req, res) {
+    const schemaUser = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await schemaUser.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validations fails.' });
+    }
+
     const { email } = req.body;
-    const user = await User.findOne({ where: { email } });
+    const user = await User.findOne({ where: { email: req.body.email } });
 
     if (!user) {
       return res.status(400).json({ error: 'User does not exist.' });
@@ -50,8 +90,6 @@ class UserController {
 
   async delete(req, res) {
     const { email } = req.body;
-    const { id } = await User.findOne({ where: { email } });
-
     const user = await User.findOne({ where: { email } });
 
     if (!user) {
@@ -59,12 +97,16 @@ class UserController {
     }
 
     // eslint-disable-next-line consistent-return
-    User.destroy({ where: { id } }).then(response => {
+    return User.destroy({ where: { id: user.id } }).then(response => {
       if (response === 1) {
-        return res.status(200).json({ message: 'User deleted successfully.' });
+        res
+          .status(200)
+          .json({ message: 'User deleted successfully.' })
+          .end();
+      } else {
+        res.status(400).json('User not deleted error');
       }
     });
-    return res.status(400).json('User not deleted error');
   }
 }
 
